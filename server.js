34d54ec36f950c231b158df9d44c37e214b6b074
zhangxuepeng <3238811454@qq.com>
2020-08-26 20:11:48 +0800

    初始化 commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const WebSocket = require('ws');
+const moment = require('moment');
+const MongoClient = require('mongodb').MongoClient
+
+
+
+//连接MongoDB数据库
+const mongoUrl = 'mongodb://localhost:27017/lening'
+//连接选项
+const mongoOption = {
+	useUnifiedTopology: true,
+	auth:{user:"lening",password:"zxpsj$123"}
+}
+const wss = new WebSocket.Server({ port: 8080 });
+
+MongoClient.connect(mongoUrl,mongoOption,function(err,client){
+
+	const db = client.db();
+	const collection = db.collection('chat_msg')	//选择集合（表）
+
+	wss.on('connection', function connection(ws) {
+		//接收客户端数据
+		ws.on('message', function incoming(message) {
+			var t = moment().format();		//当前时间
+			ws.send(message + " " + t);		//向客户端发送数据
+
+			//将数据保存至 mongodb
+			var doc = {msg:message,time:t};
+			collection.insertOne(doc,function(err,result){
+				console.log(err);
+				console.log(message + " 消息入库");
+			})
+			//广播 向所有在线用户发送数据
+			wss.clients.forEach(function each(client) {
+				if ( client !== ws && client.readyState === WebSocket.OPEN) {
+					client.send(message + " " + t);
+				}
+			});
+		});
+
+	});
+
+});
